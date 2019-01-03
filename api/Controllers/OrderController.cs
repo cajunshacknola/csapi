a8a40ce065be8d4585338307b7d8d39c11f1ae78
Hung Nguyen <anhyeast@yahoo.com>
2019-01-03 14:45:07 -0600

    ncie

---
+++ api/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Square.Connect.Api;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var locationsApi = new LocationsApi();
+            var locations = locationsApi.ListLocations();
+            return new JsonResult(locations);
+        }
+    }
+}
